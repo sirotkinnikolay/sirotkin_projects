95203b8932436d8823acb5ff3dbc5363d8f893da
Nikolay Sirotkin <sirotkin.nikola@mail.ru>
2023-01-07 22:35:38 +0300

    готово, протестировано

--- telegramm_bot_mp3_translater/src/start_bot.py
+++ telegramm_bot_mp3_translater/src/start_bot.py
@@ -1,115 +1,145 @@
 import io
 import os
-from loader import bot
-from telebot import types
 import pathlib
 from pathlib import Path
+from loader import bot
+from telebot import types
 import gtts
 from gtts import gTTS
-from playsound import playsound
-
-language = ''
-flag_file = True
-flag_text = True
+from googletrans import Translator
+from src.utils import action_text_markup, file_path,\
+    language_selection, command_choice, mp3_transformation
 
 
-def action_text_markup(message):
-    markup = types.ReplyKeyboardMarkup(True, False)
-    markup.row('перевести текстовый файл')
-    markup.add('ввести текст для перевода в ручную')
-    bot.send_message(message.from_user.id, 'выберите вариант ввода текста.',
-                     reply_markup=markup)
-    bot.send_message(message.chat.id, reply_markup=types.ReplyKeyboardRemove())
+LANGUAGE = ''
+FLAG_FILE = True
+FLAG_TEXT = True
+FLAG_TRANSLATE = False
 
 
-def file_path():
-    a = os.path.basename(__file__)
-    b = os.path.abspath(__file__).replace(a, '')
-    return b
-
-
-@bot.message_handler(func=lambda message: flag_file == True, content_types=['document'])
+@bot.message_handler(func=lambda message: FLAG_FILE is True, content_types=['document'])
 def addfile(message):
-    bot.send_message(message.chat.id, "Выберите коменду из меню "
-                                      '\n /translate - Начало работы:'
-                                      '\n /help - Помощь с использованием')
+    """При попытке загрузки текстового файла раньше его запроса, выводится стартовое меню"""
+    command_choice(message)
 
 
-@bot.message_handler(commands=['translate'])
-def language_selection(message):
-    """При выборе команды /translate, предоставляются кнопки для выбора языка переводимого текста."""
+@bot.message_handler(commands=['help'])
+def help_func(message):
+    """При выборе команды /help, выводится информация про данного бота и его работу"""
+    bot.send_message(message.from_user.id, 'Данный телеграмм-бот предназначен для конвертации'
+                                           ' текста в .mp3 файл, и дальнейшего его воспроизведения,'
+                                           ' возможет вариант ввода текста вручную'
+                                           'или возможна загрузка .txt файла, так же можно'
+                                           ' перевести текст на Русский язык перед его '
+                                           'конвертацией, доступны два языка, Английский и'
+                                           ' Немецкий, для начала работы'
+                                           ' воспользуйтесь командой  /start  и следуйте'
+                                           ' дальнейшим инструкциям')
+
+
+@bot.message_handler(commands=['start'])
+def translate(message):
+    """При запуске команды /start, запрашивается необходим перевод текста перед его конвертацией,
+     выбор реализуется кнопками 'да' и 'нет'"""
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("German")
-    btn2 = types.KeyboardButton('English')
+    btn1 = types.KeyboardButton('да')
+    btn2 = types.KeyboardButton('нет')
     markup.add(btn1, btn2)
-    bot.send_message(message.from_user.id, 'Выберите язык с которого необходимо перевести текст',
+    bot.send_message(message.from_user.id, 'Перевести на Русский язык ?',
                      reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: flag_text == True, content_types=['text'])
+@bot.message_handler(func=lambda message: FLAG_TEXT is True, content_types=['text'])
 def action_text(message):
-    """При выборе языка переводимого текста, предоставляются кнопки для выбора варианта введения текста,
-     текст можно ввести напрямую в сообщении или загрузить .txt файл с текстом для первода."""
-    global language
-    global flag_file
-    global flag_text
-    if message.text == 'German':
-        language = 'de'
+    """Обработка ввода текста с кнопок по необходимости перевода, языка с которого
+     требуется конвертация и варианта ввода текста, исходя из этого запускается
+      необходимая функция для дальнейшего выбора параметров или
+      обработки текста"""
+    global LANGUAGE
+    global FLAG_FILE
+    global FLAG_TEXT
+    global FLAG_TRANSLATE
+    translator = Translator()
+
+    if message.text == 'да':
+        FLAG_TRANSLATE = True
+        language_selection(message)
+
+    elif message.text == 'нет':
+        FLAG_TRANSLATE = False
+        language_selection(message)
+
+    elif message.text == 'German':
+        LANGUAGE = 'de'
         action_text_markup(message)
 
     elif message.text == 'English':
-        language = 'en'
+        LANGUAGE = 'en'
         action_text_markup(message)
 
-    elif message.text == 'перевести текстовый файл':
-        flag_file = False
+    elif message.text == 'воспроизвести текстовый файл':
+        FLAG_FILE = False
         bot.send_message(message.chat.id, 'выберите файл для загрузки')
 
         @bot.message_handler(content_types=['document'])
         def add_file(message):
+            """Если был выбран вариан загрузки текста .txt файлом ,
+             загружается выбранный файл и происходит
+             его конвертация, при необходимости перевод на русский язык"""
+            global LANGUAGE
             try:
                 folder_1 = Path(file_path(), 'text_file', message.document.file_name)
                 file_info = bot.get_file(message.document.file_id)
                 downloaded_file = bot.download_file(file_info.file_path)
                 with open(folder_1, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                with open(folder_1, 'r') as tx:
-                    one = tx.read()
-                mp3_file = gtts.gTTS(one, lang=language, slow=False)
-                mp3_file.save('text_file_translation.mp3')
-                mp3_path = os.path.abspath(os.path.join("text_file_translation.mp3"))
-                bot.send_audio(message.from_user.id,
-                               audio=open(mp3_path, 'rb'))
+                with open(folder_1, 'r') as file:
+                    one = file.read()
+                    if FLAG_TRANSLATE:
+                        trans = translator.translate(one, src=LANGUAGE, dest='ru')
+                        one = trans.text
+                        LANGUAGE = 'ru'
 
-            except Exception as e:
-                bot.reply_to(message, e)
+                mp3_transformation(lang=LANGUAGE, text=one)
+                mp3_path = os.path.abspath(os.path.join("text_translation.mp3"))
+                bot.send_audio(message.from_user.id, audio=open(mp3_path, 'rb'),
+                               reply_markup=types.ReplyKeyboardRemove(), parse_mode='Markdown')
+                os.remove(folder_1)
+                command_choice(message)
 
-    elif message.text == 'ввести текст для перевода в ручную':
-        flag_file = True
-        flag_text = False
-        bot.send_message(message.chat.id, 'введите текст для перевода')
+            except Exception as fail:
+                bot.reply_to(message, fail)
+
+    elif message.text == 'ввести текст для воспроизведения в ручную':
+        FLAG_FILE = True
+        FLAG_TEXT = False
+        bot.send_message(message.chat.id, 'введите текст')
 
         @bot.message_handler(content_types=['text'])
         def text_write(message):
-            global flag_text
-            global language
-            while not flag_text:
+            """Если текст введен и отправлен после его запроса, то текст
+             обрабатывается как текст для конвертации,
+             если текст введен раньше запроса, то выводится стартовое меню"""
+            global FLAG_TEXT
+            global LANGUAGE
+            while not FLAG_TEXT:
                 folder_2 = Path(file_path() + 'text_file')
                 filepath = os.path.join(folder_2, "text.txt")
+                two = message.text
+                if FLAG_TRANSLATE:
+                    trans = translator.translate(two, src=LANGUAGE, dest='ru')
+                    two = trans.text
+                    LANGUAGE = 'ru'
 
                 mp3_path = os.path.abspath(os.path.join("text_translation.mp3"))
-                mp3_file = gtts.gTTS(message.text, lang=language, slow=False)
-                mp3_file.save('text_translation.mp3')
+                mp3_transformation(lang=LANGUAGE, text=two)
 
                 with open(filepath, 'w') as file:
                     file.write(message.text)
-                    flag_text = True
-                bot.send_audio(message.from_user.id,
-                               audio=open(mp3_path, 'rb'))
-                bot.send_message(message.chat.id, reply_markup=types.ReplyKeyboardRemove())
-
+                    FLAG_TEXT = True
+                bot.send_audio(message.from_user.id, audio=open(mp3_path, 'rb'),
+                               reply_markup=types.ReplyKeyboardRemove(), parse_mode='Markdown')
+                command_choice(message)
 
     else:
-        bot.send_message(message.chat.id, 'Вас приветствует бот для перевода текста и прослушивания его.'
-                                          '\n /translate - Начало работы:'
-                                          '\n /help - Помощь с использованием')
+        command_choice(message)
