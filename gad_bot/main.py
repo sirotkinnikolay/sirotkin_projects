59c8f7c4bf7a441f12262a00bed84e8718e16463
sirotkinnikolay <95248794+sirotkinnikolay@users.noreply.github.com>
2023-01-20 19:22:11 +0300

    Delete gad_bot directory

--- gad_bot/main.py
+++
@@ -1,8 +0,0 @@
-from loader import bot
-
-while True:
-    try:
-        if __name__ == '__main__':
-            bot.polling(none_stop=True, interval=0)
-    except Exception as problem:
-        pass
